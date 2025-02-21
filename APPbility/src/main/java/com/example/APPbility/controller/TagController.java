4166b45dd4b7b9be13d80e13632c5ca354e0ff95
Lucas <falla.urluc24@triana.salesianos.edu>
2025-02-21 19:57:07 +0100

    Dependencia de documentación incluida y primer endpoint completado (a falta de documentar).

--- APPbility/src/main/java/com/example/APPbility/controller/TagController.java
+++ APPbility/src/main/java/com/example/APPbility/controller/TagController.java
@@ -2,6 +2,7 @@ package com.example.APPbility.controller;
 
 import com.example.APPbility.dto.tag.GetTagDTO;
 import com.example.APPbility.service.TagService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/tag/")
+@Tag(name = "Tag", description = "Controlador de Tag, para poder realizar sus operaciones de gestión.")
 public class TagController {
 
     private final TagService tagService;
